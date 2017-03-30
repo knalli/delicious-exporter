106eede3e0e8723b1f3e49b1742f3631ed246d23
Mladen Mihajlovic <mmihajlovic@gmail.com>
2017-03-30 14:47:45 +0200

    Added export of private data Also some minor changes and fixes

--- src/lib/DeliciousHtmlExporter.js
+++ src/lib/DeliciousHtmlExporter.js
@@ -3,6 +3,7 @@ import request from "request-promise";
 import progressbar from "progressbar";
 import HtmlParser from "./HtmlParser.js";
 import UrlVerifier from "./UrlVerifier.js";
+import CookieParser from "set-cookie-parser";
 
 class RemoteHttpLoader {
 
@@ -11,13 +12,16 @@ class RemoteHttpLoader {
     this.baseEndpoint = baseEndpoint;
   }
 
-  loadPage(page) {
-    return new Promise((resolve, reject) => {
-      request(`${this.baseEndpoint}/${this.username}?page=${page}`)
-        .then(resolve, reject);
+  loadPage(page, cookies) {
+
+    return request({
+
+        uri: `${this.baseEndpoint}/${this.username}?page=${page}`,
+        headers: {
+            'Cookie': cookies
+        }
     });
   }
-
 }
 
 class LocalFileLoader {
@@ -27,13 +31,13 @@ class LocalFileLoader {
     this.fileSuffix = fileSuffix;
   }
 
-  loadPage(page) {
+  loadPage(page, cookies=[]) {
     return new Promise((resolve, reject) => {
       fs.readFile(`${this.filePrefix}${page}${this.fileSuffix}`, 'utf8', (err, data) => {
         if (err) {
           reject({
-                   message: err.message
-                 });
+            message: err.message
+          });
         } else {
           resolve(data.toString());
         }
@@ -55,8 +59,8 @@ class LocalFileWriter {
       fs.writeFile(`${this.filePrefix}${page}${this.fileSuffix}`, data, 'utf8', (err) => {
         if (err) {
           reject({
-                   message: err.message
-                 });
+            message: err.message
+          });
         } else {
           resolve();
         }
@@ -68,10 +72,10 @@ class LocalFileWriter {
 
 class ResultVerifier {
 
-  constructor({verbose = false}, {urls = false}) {
+  constructor({ verbose = false }, { urls = false }) {
     this.verbose = verbose;
     this.urlVerifier = urls
-      ? new UrlVerifier({verbose: verbose})
+      ? new UrlVerifier({ verbose: verbose })
       : null;
   }
 
@@ -79,22 +83,22 @@ class ResultVerifier {
     const progress = progressbar.create().step('item result validation');
     progress.setTotal(items.length);
     return Promise.all(items.map(item => this.verifyItem(item, progress)))
-                  .then(() => {
-                    progress.finish();
-                    return items;
-                  });
+      .then(() => {
+        progress.finish();
+        return items;
+      });
   }
 
   verifyItem(item, progress) {
     return Promise.all([
-                         this.verifyItem_url(item)
-                       ])
-                  .then(() => {
-                    if (progress) {
-                      progress.addTick();
-                    }
-                    return item;
-                  });
+      this.verifyItem_url(item)
+    ])
+      .then(() => {
+        if (progress) {
+          progress.addTick();
+        }
+        return item;
+      });
   }
 
   verifyItem_url(item) {
@@ -110,14 +114,14 @@ class ResultVerifier {
         console.log(`Verify item ${item.id} '${item.title}: ${item.url}'`);
       }
       this.urlVerifier.verify(item.url)
-          .then(applyResult, applyResult)
-          .then(() => {
-            if (this.verbose) {
-              console.log(`Verify item ${item.id} '${item.title}: ${item.url}' COMPLETED`);
-            }
-            resolve(item);
-          })
-          .catch(() => resolve(item));
+        .then(applyResult, applyResult)
+        .then(() => {
+          if (this.verbose) {
+            console.log(`Verify item ${item.id} '${item.title}: ${item.url}' COMPLETED`);
+          }
+          resolve(item);
+        })
+        .catch(() => resolve(item));
     });
   }
 
@@ -125,8 +129,14 @@ class ResultVerifier {
 
 class DeliciousHtmlExporter {
 
-  constructor({username, baseEndpoint = 'https://del.icio.us', writeHtmlToDirectory = false, readHtmlFromDirectory = false, validityOptions = {}}) {
+  constructor({ username, password, baseEndpoint = 'https://del.icio.us', writeHtmlToDirectory = false, readHtmlFromDirectory = false, validityOptions = {} }) {
     this.verbose = false;
+    this.username = username;
+    this.password = password;
+    this.baseEndpoint = baseEndpoint;
+    this.readHtmlFromDirectory = readHtmlFromDirectory;
+    this.cookies = '';
+
     if (readHtmlFromDirectory) {
       this.loader = new LocalFileLoader(`${readHtmlFromDirectory}/page_`, '.html');
     } else {
@@ -139,62 +149,107 @@ class DeliciousHtmlExporter {
       this.writer = new LocalFileWriter(`${writeHtmlToDirectory}/page_`, '.html');
     }
     this.parser = new HtmlParser();
-    this.verifier = new ResultVerifier({verbose: this.verbose}, validityOptions);
+    this.verifier = new ResultVerifier({ verbose: this.verbose }, validityOptions);
 
-    console.log(`Starting del.icio.us HTML exporter for '${username}' @ '${baseEndpoint}'`);
-    console.log('  - reading from ' + (readHtmlFromDirectory ? (`<local: '${readHtmlFromDirectory}'>`) : `<remote: ${baseEndpoint}>` ));
+    console.log(`Starting del.icio.us HTML exporter for '${username}:${password}' @ '${baseEndpoint}'`);
+    console.log('  - reading from ' + (readHtmlFromDirectory ? (`<local: '${readHtmlFromDirectory}'>`) : `<remote: ${baseEndpoint}>`));
     console.log('  - writing results ' + (writeHtmlToDirectory ? (`yes <local: ${writeHtmlToDirectory}>`) : 'no'));
     console.log(`  - validating results (urls=${validityOptions.urls ? 'yes' : 'no'})`);
   }
 
+  login() {
+      const exporter = this;
+
+      return new Promise((resolve, reject) => {
+        if (this.password && !this.readHtmlFromDirectory) {
+
+          console.log('Logging in...');
+
+          const progress = progressbar.create();
+
+          request({
+            method: 'POST',
+            uri: `${this.baseEndpoint}/login`,
+            form: {
+              username: this.username,
+              password: this.password
+            },
+            resolveWithFullResponse: true
+          })
+            .then(function (response) {
+              let _cookies = CookieParser.parse(response);
+
+              //also get session field from json result
+              var body = JSON.parse(response.body);
+              var session = body.session.split('=');
+              _cookies.push({
+                name:session[0],
+                value:session[1]
+              })
+
+              exporter.cookies = _cookies.reduce((acc, curr) => acc + (curr ? `${curr.name}=${curr.value};` : ''), '');
+
+            })
+            .then(resolve, reject);
+        }
+        else {
+          resolve();
+        }
+      });
+    }
+  
+
   fetch() {
     return new Promise((resolve, reject) => {
       const progress = progressbar.create();
+
       this.fetchPage()
-          .then(response => {
-            let combinedResult = {
-              title: response.title,
-              pages: response.page.total,
-              totalElements: response.page.totalElements,
-              items: []
-            };
-            console.log(`Fetched initial page, and now we know how many to load: ${response.page.total} pages, ${response.page.totalElements} items`);
-            progress.step('page fetching')
-                    .setTotal(response.page.total);
-            return this.collectAndFetchNext(response, combinedResult, progress);
-          })
-          .then(result => {
-            progress.finish();
-            if (this.verifier) {
-              if (this.verbose) {
-                console.log(`Now verifying all ${result.items.length} items...`);
-              }
-              return this.verifier
-                         .verify(result.items)
-                         .then(() => result, () => result);
-            } else {
-              return result;
+        .then(response => {
+          let combinedResult = {
+            title: response.title,
+            pages: response.page.total,
+            totalElements: response.page.totalElements,
+            items: []
+          };
+          console.log(`Fetched initial page, and now we know how many to load: ${response.page.total} pages, ${response.page.totalElements} items`);
+          progress.step('page fetching')
+            .setTotal(response.page.total);
+          return this.collectAndFetchNext(response, combinedResult, progress);
+        })
+        .then(result => {
+          progress.finish();
+          if (this.verifier) {
+            if (this.verbose) {
+              console.log(`Now verifying all ${result.items.length} items...`);
             }
-          })
-          .then(resolve, reject);
+            return this.verifier
+              .verify(result.items)
+              .then(() => result, () => result);
+          } else {
+            return result;
+          }
+        })
+        .then(resolve, reject);
     });
   }
 
   fetchPage(page = 1) {
-    return this.loader.loadPage(page)
-               .then((html) => {
-                 if (this.writer) {
-                   this.writer.savePage(page, html)
-                       .catch(error => console.error('Failed to write page due: ' + error.message));
-                 }
-                 return this.parser.parse(html);
-               })
-               .then(response => {
-                 if (this.verbose) {
-                   console.log(`Fetched page ${response.page.number}/${response.page.total}`);
-                 }
-                 return response;
-               });
+    const cookies = this.cookies; 
+
+    return this.loader.loadPage(page, cookies)
+      .then((html) => {
+        if (this.writer) {
+          this.writer.savePage(page, html)
+            .catch(error => console.error('Failed to write page due: ' + error.message));
+        }
+        return this.parser.parse(html);
+      })
+      .then(response => {
+        if (this.verbose) {
+          console.log(`Fetched page ${response.page.number}/${response.page.total}`);
+        }
+        return response;
+      })
   }
 
   collectAndFetchNext(currentResponse, combinedResult, progress) {
@@ -205,8 +260,8 @@ class DeliciousHtmlExporter {
       combinedResult.items.push(...currentResponse.items);
       if (currentResponse.page.next) {
         this.fetchPage(currentResponse.page.next)
-            .then(nextResponse => this.collectAndFetchNext(nextResponse, combinedResult, progress))
-            .then(resolve, reject);
+          .then(nextResponse => this.collectAndFetchNext(nextResponse, combinedResult, progress))
+          .then(resolve, reject);
       } else {
         resolve(combinedResult);
       }
